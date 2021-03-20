daa4d1c74daadfa0cf578bdb78b4ac6af0013488
mrinex <mrinex@mail.ru>
2021-03-21 00:48:01 +0300

    feat: add array destructuring

---
+++ Chp_02/02_07/app.js
@@ -0,0 +1,2 @@
+var array = [123, "Pick up drycleaning", false];
+var id = array[0], title = array[1], completed = array[2];
---
+++ Chp_02/02_07/app.ts
@@ -0,0 +1,2 @@
+const array = [123, "Pick up drycleaning", false];
+let [id, title, completed] = array;
